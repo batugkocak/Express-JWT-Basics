99323e85931a0f4ee092417f2f4322a8d44a8969
Batug Kocak <batugkocak@gmail.com>
2024-05-01 18:13:24 +0300

    feat: Add basic JWT implementation

--- controllers/main.js
+++ controllers/main.js
@@ -3,15 +3,29 @@
 // send back to front-end
 
 // setup authentication so only the request with JWT can access the dasboard
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+require("dotenv").config();
 
 const login = async (req, res, next) => {
-  res.send("Fake Login Route");
+  const { username, password } = req.body;
+  // mongoose validation
+  // Joi
+  // check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide an email and password!", 400);
+  }
+  const token = jwt.sign({ username: username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Hello, John Doe`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your luck number is ${luckyNumber}`,
   });
 };
